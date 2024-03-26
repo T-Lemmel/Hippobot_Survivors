424286edc8dfbc717aa6fe86d105d67479802231
Gauthier <148333942+GauthierBertram@users.noreply.github.com>
2024-03-26 14:29:32 +0100

    Add files via upload

--- mapping/src/FillMap.cpp
+++ mapping/src/FillMap.cpp
@@ -3,14 +3,15 @@
 #include <rclcpp/node.hpp>
 #include <geometry_msgs/msg/pose_array.hpp>
 #include <geometry_msgs/msg/pose.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <nav_msgs/msg/occupancy_grid.hpp>
 #include <map_msgs/msg/occupancy_grid_update.hpp>
 #include <iostream>
 #include <ctime>
 #include <my_robot_interfaces/msg/centroid_with_radius.hpp>
 #include <my_robot_interfaces/msg/centroid_array_with_radius.hpp>
-#include <tf2_ros/static_transform_broadcaster.h>
-#include <tf2/LinearMath/Quaternion.h>
+//#include <tf2_ros/static_transform_broadcaster.h>
+//#include <tf2/LinearMath/Quaternion.h>
 
 // TO DO : Make sure the update topic doesn't publish out of bounds and reduce initial map publishing time ( ligne 36-41 )
 
@@ -21,8 +22,8 @@ public:
 
             subscription_ = this->create_subscription<my_robot_interfaces::msg::CentroidArrayWithRadius>(
                 "centroids", 10, std::bind(&Mapping::UpdateCentroids, this, std::placeholders::_1));
-            subscription2_= this->create_subscription<geometry_msgs::msg::Pose>(
-                        "boat_pose", 10, std::bind(&Mapping::BoatPoseCallback, this, std::placeholders::_1));
+            subscription2_= this->create_subscription<nav_msgs::msg::Odometry>(
+                        "wamv/odom", 10, std::bind(&Mapping::BoatPoseCallback, this, std::placeholders::_1));
             
             
             MapPublisher_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("WorldMap", 10); //TO DO use transient local instead of volatile 
@@ -44,6 +45,7 @@ public:
 
 private:
 
+
     void MapCreate()
     {
         std::cout << "map creation"<<std::endl;
@@ -60,9 +62,9 @@ private:
     }
 
 
-    void BoatPoseCallback(const geometry_msgs::msg::Pose::SharedPtr msg ){
-        boat_X = msg->position.x;
-        boat_Y = msg->position.y;
+    void BoatPoseCallback(const nav_msgs::msg::Odometry::SharedPtr msg ){
+      posX = msg->pose.pose.position.x;
+      posY = msg->pose.pose.position.y;
     }
 
 
@@ -71,6 +73,18 @@ private:
       {
 
         std::cout << "traitement centroides";
+        if (count == refresh)
+        {
+            UpdateMap(posX,posY,100,0);
+            //UpdateMap(posX,posY,1,-1);
+            count = 0;
+        }
+        else
+        {
+            count=count+1;
+        }
+
+
         for (const my_robot_interfaces::msg::CentroidWithRadius& centroid : CentroidsWithRadius->centroids)
         {
           auto center = centroid.position;
@@ -78,21 +92,13 @@ private:
           int x = (int)center.x;
           int y = (int)center.y;
           int r = (int)radius;
-          UpdateMap(x,y,r,1);
+          if (-100<x-posX && x-posX<100 && -100<y-posY && y-posY<100)
+          {
+            UpdateMap(x,y,r,1);
+          }
         }
-        UpdateBoat();
       }
 
-
-
-    void UpdateBoat()
-      {
-        UpdateMap(oldposx,oldposy,1,0);
-        oldposx = (int)boat_X;
-        oldposy = (int)boat_Y,
-        UpdateMap(oldposx,oldposy,1,-1);
-        
-      }
 
 
 
@@ -115,15 +121,17 @@ private:
 
 
       rclcpp::Subscription<my_robot_interfaces::msg::CentroidArrayWithRadius>::SharedPtr subscription_;
-      rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr subscription2_;
+      rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription2_;
       rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr MapPublisher_;
       rclcpp::Publisher<map_msgs::msg::OccupancyGridUpdate>::SharedPtr MapUpdatePublisher_;
       nav_msgs::msg::OccupancyGrid map_;
 
-      int oldposx = 500;
-      int oldposy = 500;
-      int boat_X = 500;
-      int boat_Y = 500;
+
+      int posX = 0;
+      int posY = 0;
+
+      int count = 0;
+      int refresh = 10;
 };
 
 int main(int argc, char** argv) {
